83793bf47bdfcaeda6a6ff6e6fc3b64813eab601
Константин <kostay_mc@mail.ru>
2022-11-23 18:09:01 +0300

    first task

--- hometask0/Program.cs
+++ hometask0/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("введите первое число:");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("введите второе число:");
+int b = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if(a>b) max=a;
+else max = b;
+
+Console.Write(max);
