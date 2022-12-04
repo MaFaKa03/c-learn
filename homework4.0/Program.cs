84b08b7af77594b4ff04886460f0be9011e2c96f
Константин <kostay_mc@mail.ru>
2022-12-04 21:10:47 +0300

    first task

---
+++ homework4.0/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 1: Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B. Без модуля Math, используем цикл for.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+void num(int A, int B)
+
+{
+    int result = A;
+    for (int count = 1;count < B; count++)
+    {
+        result = result * A;
+    }
+    Console.WriteLine(result);
+}   
+
+Console.WriteLine("Введите число");
+int A=int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число");
+int B=int.Parse(Console.ReadLine());
+num(A,B);
