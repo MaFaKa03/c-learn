796bb9dd2a64171cb05a657879ef467151c941c8
Константин <kostay_mc@mail.ru>
2022-12-06 19:22:15 +0300

    first task complate

--- 5.0/Program.cs
+++ 5.0/Program.cs
@@ -10,14 +10,33 @@ void Print(int[] arr)
 	    Console.WriteLine();
 	}
 	
-	int[] EightMass(int size)
+	int[] MassNums(int size, int from, int to)
 	{
 	    int[] arr = new int[size];
 	
 	    for (int i = 0; i < size; i++)
-	        arr[i] = new Random().Next(1, 100);
+	        arr[i] = new Random().Next(from, to);
 	    return arr;
 	}
 	
-	int[] arr_1 = EightMass(int.Parse(Console.ReadLine()));
+	void SumPosNeg(int[] arr)
+	{
+	    int pos, neg;
+	    pos = neg = 0;
+	
+	    for (int i = 0; i < arr.Length; i++)
+	    {
+	        if (arr[i] >= 0)
+	            pos += arr[i];
+	        else
+	            neg += arr[i];
+	    }
+	    Console.WriteLine($"Positive: {pos}, negative: {neg}");
+	
+	}
+	
+	int[] arr_1 = MassNums(int.Parse(Console.ReadLine()),
+	                       int.Parse(Console.ReadLine()),
+	                       int.Parse(Console.ReadLine()));
 	Print(arr_1);
+	SumPosNeg(arr_1);
