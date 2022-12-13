7217fee4e8584185e36fad82788fda77a3e8f8dc
Константин <kostay_mc@mail.ru>
2022-12-13 18:32:12 +0300

    second task compl

---
+++ homework6.1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая найдёт точку пересечения двух прямых,
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void Dote(double k1, double b1, double k2, double b2)
+{
+    double x = (b2 - b1) / (k1 -k2); 
+    double y = k1 * x + b1;
+    double y2 = k2 * x + b2;
+    
+    if(x == y && x == y2 && y == y2)
+    Console.WriteLine($"{x};{y}");
+    else Console.WriteLine("Точки не пересекаются");
+}
+
+Dote(int.Parse(Console.ReadLine()),
+    int.Parse(Console.ReadLine()),
+    int.Parse(Console.ReadLine()),
+    int.Parse(Console.ReadLine()));
+                        
